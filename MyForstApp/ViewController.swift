0867a44c1b73fc1b7104efdcbfa7eb391fd322d8
skrall3119 <alexjanci@yahoo.com>
2020-08-26 15:22:47 -0500

    Initial Commit

---
+++ MyForstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyForstApp
+//
+//  Created by Alex Janci on 8/26/20.
+//  Copyright © 2020 Alex Janci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
